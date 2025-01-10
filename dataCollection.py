ea682fadaa2b0a1e21dd4f771d12fc51396f182c
Agrim Bansal <agrimbansal12@gmail.com>
2025-01-10 20:22:53 +0530

    I've modified the py script to get the stock pricing data from web. I'll turn it on and let it be in a few.

--- dataCollection.py
+++ dataCollection.py
@@ -13,42 +13,81 @@ load_dotenv('.env')
 API_KEY = os.environ.get('FINNHUB_API_KEY')
 
 # Function to get the data from the api
+# def get_data(symbol):
+#     url = f'https://finnhub.io/api/v1/stock/profile2?symbol={symbol}'
+
+#     response = requests.get(url, headers={'X-Finnhub-Token': API_KEY})
+    
+
+#     data = response.json()
+#     return data
+
 def get_data(symbol):
-    url = f'https://finnhub.io/api/v1/stock/profile2?symbol={symbol}'
+    url = f'https://finnhub.io/api/v1/stock//quote?symbol={symbol}'
 
     response = requests.get(url, headers={'X-Finnhub-Token': API_KEY})
     
 
     data = response.json()
-    return data
-
-symbolList = pd.read_excel('./allSymbols.xlsx')
-
-# symbolInfo = pd.DataFrame(columns=['symbol', 'logoUrl', 'name', 'marketCap', 'volume', 'industry'])
-symbolInfo = pd.read_excel('./symbolInfo.xlsx')
+    return [data['c'], data['c'] - data['pc']]
 
 
-for symbol in symbolList['symbol'][2750:]:
+symbolList = pd.read_excel('./src/data/allSymbols.xlsx')
+
+symbolInfo = pd.DataFrame(columns=['symbol', 'logoUrl', 'name', 'marketCap', 'volume', 'industry', 'price' ,'change'])
+# symbolInfo = pd.read_excel('./symbolInfo.xlsx')
+
+
+# for symbol in symbolList:
+#     try:
+#         print(f'Getting data for {symbol}')
+#         data = get_data(symbol)
+#         newData = {
+#             'symbol': symbol,
+#             'logoUrl' : data['logo'],
+#             'name': data['name'],
+#             'marketCap': data['marketCapitalization'],
+#             'volume' : data['shareOutstanding'],
+#             'industry': data['finnhubIndustry'],
+#         }
+#         symbolInfo = pd.concat([symbolInfo, pd.DataFrame([newData])], ignore_index=True)
+#         time.sleep(1)
+#         symbolInfo.to_excel('./symbolInfo.xlsx', index=False)
+#     except KeyboardInterrupt:
+#         break
+#     except Exception as e:
+#         print(f'Error in getting data for {symbol} : {e}')
+# else:
+#     symbolInfo.to_excel('./symbolInfo.xlsx', index=False)
+#     print('Data Collection Done!')
+    
+# print(symbolList['symbol'][2750])
+
+
+for index, row in symbolList.iterrows():
     try:
-        print(f'Getting data for {symbol}')
-        data = get_data(symbol)
+        data = get_data(row['symbol'])
+
         newData = {
-            'symbol': symbol,
-            'logoUrl' : data['logo'],
-            'name': data['name'],
-            'marketCap': data['marketCapitalization'],
-            'volume' : data['shareOutstanding'],
-            'industry': data['finnhubIndustry'],
+            'symbol': row['symbol'],
+            'logoUrl' : row['logoUrl'],
+            'name': row['name'],
+            'marketCap': row['marketCap'],
+            'volume' : row['volume'],
+            'industry': row['industry'],
+            'price': data[0],
+            'change': data[1]
         }
         symbolInfo = pd.concat([symbolInfo, pd.DataFrame([newData])], ignore_index=True)
         time.sleep(1)
-        symbolInfo.to_excel('./symbolInfo.xlsx', index=False)
+        symbolInfo.to_excel('./src/data/symbolInfo.xlsx', index=False)
+        symbolInfo.to_json('./src/data/symbolInfo.json')
     except KeyboardInterrupt:
         break
     except Exception as e:
         print(f'Error in getting data for {symbol} : {e}')
 else:
-    symbolInfo.to_excel('./symbolInfo.xlsx', index=False)
+    symbolInfo.to_excel('./src/data/symbolInfo.xlsx', index=False)
+    symbolInfo.to_json('./src/data/symbolInfo.json')
     print('Data Collection Done!')
     
-# print(symbolList['symbol'][2750])
