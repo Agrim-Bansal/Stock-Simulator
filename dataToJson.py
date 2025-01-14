65c0ef39a87b3d63b4ac4f5d3c65a8354d64e24a
Agrim Bansal <agrimbansal12@gmail.com>
2025-01-14 21:55:47 +0530

    Added a comment on the scripts that they can be removed without issues

--- dataToJson.py
+++ dataToJson.py
@@ -1,5 +1,7 @@
 '''
 This file is going to create a dataset for the table of stocks. The data with apis is not available in the form i need for free. so i'll manually have to scrap and make this.
+This is not used in the project and is only a data scrapping script which has been used to create the dataset for the project.
+So, this file can safely be deleted without problems.
 '''
 
 # Importing the required libraries
